fbb0dee733a28f7b28305ee73067a47a395891a5
Mauricio Duarte <prussdev@gmail.com>
2023-05-11 20:10:04 -0300

    fix fields new user

--- lib/src/modules/user/presentation/pages/new_user_page.dart
+++ lib/src/modules/user/presentation/pages/new_user_page.dart
@@ -37,93 +37,98 @@ class _NewUserPageState extends State<NewUserPage> {
             },
             icon: const Icon(Icons.home)),
       ),
-      body: Form(
-        key: controller.formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: ListView(
-            children: [
-              const Padding(padding: EdgeInsets.all(10.0)),
-              CustomTextField(
-                label: 'Nome completo',
-                icon: Icons.person,
-                hint: 'Digite seu nome',
-                initialValue: controller.user.fullname ?? '',
-                validators: [requiredValidator],
-                onSave: (text) => controller.change(fullname: text),
+      body: ValueListenableBuilder(
+        valueListenable: controller.user,
+        builder: (context, user, child) {
+          return Form(
+            key: controller.formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ListView(
+                children: [
+                  const Padding(padding: EdgeInsets.all(10.0)),
+                  CustomTextField(
+                    label: 'Nome completo',
+                    icon: Icons.person,
+                    hint: 'Digite seu nome',
+                    initialValue: user.fullname ?? '',
+                    validators: [requiredValidator],
+                    onSave: (text) => controller.change(fullname: text),
+                  ),
+                  CustomTextField(
+                    label: 'Telefone',
+                    hint: 'Digite o telefone',
+                    initialValue: user.phone?.toString() ?? '',
+                    icon: Icons.phone,
+                    onSave: (text) => controller.change(phone: text),
+                    inputFormatters: [TelefoneInputFormatter()],
+                    validators: [requiredValidator],
+                  ),
+                  CustomTextField(
+                    label: 'CEP',
+                    hint: 'Digite o CEP',
+                    icon: Icons.house,
+                    initialValue: user.cep?.toString() ?? '',
+                    validators: [requiredValidator],
+                    onSave: (text) => controller.change(cep: text),
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly, CepInputFormatter()],
+                  ),
+                  CustomTextField(
+                    label: 'Número',
+                    hint: 'Digite o número',
+                    validators: [requiredValidator],
+                    initialValue: user.number?.toString() ?? '',
+                    onSave: (text) => controller.change(number: text),
+                  ),
+                  CustomTextField(
+                    label: 'Complemento',
+                    hint: 'Digite o complemento',
+                    initialValue: user.complement ?? '',
+                    onSave: (text) => controller.change(complement: text),
+                  ),
+                  CustomTextField(
+                    label: 'Login',
+                    hint: 'Digite o login',
+                    icon: Icons.person,
+                    validators: [requiredValidator],
+                    onSave: (text) => controller.change(login: text),
+                    initialValue: user.login ?? '',
+                  ),
+                  CustomTextField(
+                    label: 'Senha',
+                    hint: 'Digite a senha',
+                    icon: Icons.key,
+                    validators: [requiredValidator],
+                    initialValue: '',
+                    onChanged: (text) => _passwordCache = text,
+                    onSave: (text) => controller.change(password: text),
+                    password: true,
+                  ),
+                  CustomTextField(
+                    label: 'Confirme a senha',
+                    hint: 'Confirme a senha',
+                    icon: Icons.key,
+                    initialValue: '',
+                    validators: [requiredValidator, passwordConfirmValidator],
+                    onChanged: (text) => _confirmPasswordCache = text,
+                    password: true,
+                  ),
+                  SizedBox(
+                    // width: double.infinity,
+                    // height: 50,
+                    child: ElevatedButton.icon(
+                      label: const Text('Salvar'),
+                      icon: const Icon(Icons.save),
+                      onPressed: () {
+                        controller.create();
+                      },
+                    ),
+                  )
+                ],
               ),
-              CustomTextField(
-                label: 'Telefone',
-                hint: 'Digite o telefone',
-                initialValue: controller.user.phone?.toString() ?? '',
-                icon: Icons.phone,
-                onSave: (text) => controller.change(phone: text),
-                inputFormatters: [TelefoneInputFormatter()],
-                validators: [requiredValidator],
-              ),
-              CustomTextField(
-                label: 'CEP',
-                hint: 'Digite o CEP',
-                icon: Icons.house,
-                initialValue: controller.user.cep?.toString() ?? '',
-                validators: [requiredValidator],
-                onSave: (text) => controller.change(cep: text),
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly, CepInputFormatter()],
-              ),
-              CustomTextField(
-                label: 'Número',
-                hint: 'Digite o número',
-                validators: [requiredValidator],
-                initialValue: controller.user.number?.toString() ?? '',
-                onSave: (text) => controller.change(number: text),
-              ),
-              CustomTextField(
-                label: 'Complemento',
-                hint: 'Digite o complemento',
-                initialValue: controller.user.complement ?? '',
-                onSave: (text) => controller.change(complement: text),
-              ),
-              CustomTextField(
-                label: 'Login',
-                hint: 'Digite o login',
-                icon: Icons.person,
-                validators: [requiredValidator],
-                onSave: (text) => controller.change(login: text),
-                initialValue: controller.user.login ?? '',
-              ),
-              CustomTextField(
-                label: 'Senha',
-                hint: 'Digite a senha',
-                icon: Icons.key,
-                validators: [requiredValidator],
-                initialValue: '',
-                onChanged: (text) => _passwordCache = text,
-                onSave: (text) => controller.change(password: text),
-                password: true,
-              ),
-              CustomTextField(
-                label: 'Confirme a senha',
-                hint: 'Confirme a senha',
-                icon: Icons.key,
-                initialValue: '',
-                validators: [requiredValidator, passwordConfirmValidator],
-                onChanged: (text) => _confirmPasswordCache = text,
-                password: true,
-              ),
-              SizedBox(
-                // width: double.infinity,
-                // height: 50,
-                child: ElevatedButton.icon(
-                  label: const Text('Salvar'),
-                  icon: const Icon(Icons.save),
-                  onPressed: () {
-                    controller.create();
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
